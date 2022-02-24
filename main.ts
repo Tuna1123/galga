ae43553b0bc2034410c2a064e73705f8dd59ce9f
Tuna1123 <99210398+Tuna1123@users.noreply.github.com>
2022-02-24 13:44:39 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,9 +25,11 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy()
+    otherSprite.destroy(effects.fire, 500)
     scene.cameraShake(4, 500)
     info.changeLifeBy(-1)
+    music.bigCrash.play()
+    spacePlane.startEffect(effects.fire)
 })
 let bogey: Sprite = null
 let projectile: Sprite = null
@@ -52,7 +54,7 @@ spacePlane = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(spacePlane, 100, 100)
 spacePlane.setStayInScreen(true)
-info.setLife(3)
+info.setLife(5)
 game.onUpdateInterval(500, function () {
     bogey = sprites.create(img`
         . . . . . . . . . . . . . . . . 
